059d18853ccf41d2790b9cdfee79d3440f7cb684
Pablo Techera <ptecherasosa@icloud.com>
2025-05-29 19:16:04 +0200

    delete ficheros mockaroo

--- tienda_online_db/categoria.sql
+++
@@ -1,5 +0,0 @@
-insert into categoria (id_categoria, nombre_categoria) values (1, 'Electronica');
-insert into categoria (id_categoria, nombre_categoria) values (2, 'Moda');
-insert into categoria (id_categoria, nombre_categoria) values (3, 'Deportes');
-insert into categoria (id_categoria, nombre_categoria) values (4, 'Libros');
-insert into categoria (id_categoria, nombre_categoria) values (5, 'Hogar');
--- tienda_online_db/estadopedido.sql
+++
@@ -1,3 +0,0 @@
-insert into estadopedido (id_estado, nombre_estado) values (1, 'Enviado');
-insert into estadopedido (id_estado, nombre_estado) values (2, 'Entregado');
-insert into estadopedido (id_estado, nombre_estado) values (3, 'Pendiente');
